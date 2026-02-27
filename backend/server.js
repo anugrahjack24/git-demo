13508bb1b8d703912369de82957b54040893babf
anugrahjack24 <anugrah.jack24@gmail.com>
2026-02-28 00:38:11 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require("path");
 require("dotenv").config();
 
 const app = express();
@@ -8,22 +9,24 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("MongoDB Connected"))
-.catch(err => console.log(err));
-
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
 
 // Routes
 const studentRoutes = require("./routes/studentRoutes");
-
 app.use("/api", studentRoutes);
 
+// Serve React Frontend
+app.use(express.static(path.join(__dirname, "../frontend2/frontend/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend3/frontend/build", "index.html"));
+});
 
 // Server
 const PORT = process.env.PORT || 10000;
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
